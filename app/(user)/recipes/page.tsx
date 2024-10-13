9709ae36f3ad50fbcd100e6029bd1dad028365ed
Kuba Parol <99417488+kubaparol@users.noreply.github.com>
2024-10-13 18:49:46 +0200

    Feature/recipes page (#2)
    * ft: add first version of recipes page
    
    * rf: remove unused code and temp commented out sections in recipes page

--- app/(user)/recipes/page.tsx
+++ app/(user)/recipes/page.tsx
@@ -1,3 +1,4 @@
+import Search from "@/components/base/Search";
 import { Button } from "@/components/ui/button";
 import { appPageTitles, ProjectUrls } from "@/constants";
 import { PlusIcon } from "lucide-react";
@@ -10,13 +11,48 @@ export const metadata: Metadata = {
 
 export default function RecipesPage() {
   return (
-    <section>
-      <Button asChild className="ml-auto">
-        <Link href={ProjectUrls.createRecipe} className="gap-3">
-          Create Recipe
-          <PlusIcon className="size-5" />
-        </Link>
-      </Button>
+    <section className="grid gap-24">
+      <header className="flex flex-col-reverse items-center gap-4 sm:flex-row sm:gap-8">
+        <Search placeholder="Search recipes..." />
+
+        <Button asChild className="w-full sm:w-fit">
+          <Link href={ProjectUrls.createRecipe} className="gap-3">
+            Create Recipe
+            <PlusIcon className="size-5" />
+          </Link>
+        </Button>
+      </header>
+
+      <div className="grid gap-10">
+        {/* <ul className="grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-x-8 gap-y-16">
+          {[].map((m) => (
+            <li key={m.id} className="bg-muted rounded-xl">
+              <header className="pb-2 grid gap-6">
+                <Image
+                  src={m.image}
+                  width={150}
+                  height={150}
+                  alt={`${m.name} picture`}
+                  className="rounded-full -mt-12 mx-auto border-2 shadow-xl"
+                />
+                <p className="text-center font-bold">{m.name}</p>
+              </header>
+
+              <Separator />
+
+              <footer className="flex items-center justify-between p-3">
+                <div className="flex items-center gap-1 text-muted-foreground">
+                  <Clock9 className="size-4" />
+                  <p className="text-xs font-medium">{m.cookTime} minutes</p>
+                </div>
+                <Button size="sm">View recipe</Button>
+              </footer>
+            </li>
+          ))}
+        </ul> */}
+
+        {/* <Pagination totalPages={10} className="mx-auto" /> */}
+      </div>
     </section>
   );
 }
---
+++ components/base/Search.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { Search as SearchIcon } from "lucide-react";
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
+import { useCallback } from "react";
+import { Input } from "../ui/input";
+// import { useDebouncedCallback } from "use-debounce";
+
+interface SearchProps {
+  placeholder: string;
+}
+
+export default function Search(props: SearchProps) {
+  const { placeholder } = props;
+
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  // const handleSearch = useDebouncedCallback((term) => {
+  const handleSearch = useCallback(
+    (term: string) => {
+      const params = new URLSearchParams(searchParams);
+
+      params.set("page", "1");
+
+      if (term) {
+        params.set("query", term);
+      } else {
+        params.delete("query");
+      }
+      replace(`${pathname}?${params.toString()}`);
+    },
+    [pathname, replace, searchParams]
+  );
+  // }, 300);
+
+  return (
+    <Input
+      startIcon={SearchIcon}
+      placeholder={placeholder}
+      onChange={(e) => {
+        handleSearch(e.target.value);
+      }}
+      defaultValue={searchParams.get("query")?.toString()}
+    />
+  );
+}
