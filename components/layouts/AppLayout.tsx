0bfaa8babb91385c37d7a117a9cb3168d53db12f
Kuba Parol <99417488+kubaparol@users.noreply.github.com>
2024-10-19 14:26:05 +0200

    rf:  refactor app layout (#5)

--- components/layouts/AppLayout.tsx
+++ components/layouts/AppLayout.tsx
@@ -8,8 +8,6 @@ import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
 import { ProjectUrls } from "@/constants/urls";
 import Logo from "../base/Logo";
 import Sidebar from "../base/Sidebar";
-import { Separator } from "@radix-ui/react-separator";
-import PageTitle from "../base/PageTitle";
 
 interface AppLayoutProps {
   readonly children: ReactNode;
@@ -23,46 +21,48 @@ export default function AppLayout(props: AppLayoutProps) {
 
   return (
     <>
-      <header className="border-b">
-        <div className="wrapper !max-w-full flex items-center gap-4 justify-between md:justify-start">
-          <Sheet open={isOpen} onOpenChange={setIsOpen}>
-            <SheetTrigger className="md:hidden">
-              <Menu />
-            </SheetTrigger>
+      <div className="border-b flex justify-between py-2 px-6 md:hidden">
+        <Link href={ProjectUrls.home} title="Cookly Home">
+          <Logo className="max-w-[100px]" />
+        </Link>
 
-            <SheetContent side="left">
-              <Sidebar
-                topItems={topItems}
-                bottomItems={bottomItems}
-                onClose={() => setIsOpen(false)}
-              />
-            </SheetContent>
-          </Sheet>
+        <Sheet open={isOpen} onOpenChange={setIsOpen}>
+          <SheetTrigger className="md:hidden ml-auto">
+            <Menu />
+          </SheetTrigger>
 
-          <div className="flex gap-2">
-            <div className="md:min-w-44">
-              <Link href={ProjectUrls.home} title="Cookly Home">
-                <Logo />
-              </Link>
-            </div>
+          <SheetContent side="right">
+            <Sidebar
+              topItems={topItems}
+              bottomItems={bottomItems}
+              onClose={() => setIsOpen(false)}
+            />
+          </SheetContent>
+        </Sheet>
+      </div>
 
-            <Separator className="w-1 bg-primary hidden md:block" />
+      <div className="flex">
+        <div className="hidden md:flex flex-col bg-[#fefae8] md:w-64">
+          <Link
+            href={ProjectUrls.home}
+            title="Cookly Home"
+            className="grid place-items-center p-4"
+          >
+            <Logo className="max-w-[140px]" />
+          </Link>
 
-            <PageTitle />
-          </div>
+          <Sidebar
+            topItems={topItems}
+            bottomItems={bottomItems}
+            onClose={() => setIsOpen(false)}
+            className="hidden md:grid flex-1"
+          />
         </div>
-      </header>
 
-      <main className="flex md:min-h-[calc(100vh-(68px+1px))] min-h-[calc(100vh-(64px+1px))]">
-        <Sidebar
-          topItems={topItems}
-          bottomItems={bottomItems}
-          onClose={() => setIsOpen(false)}
-          className="hidden md:grid"
-        />
-
-        <div className="wrapper !m-0 !mx-auto">{children}</div>
-      </main>
+        <div className="min-h-[calc(100vh-(56px))] md:min-h-[calc(100vh)] px-3 py-4 sm:px-6 flex-1">
+          {children}
+        </div>
+      </div>
     </>
   );
 }
