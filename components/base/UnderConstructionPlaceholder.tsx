fe8f64cc8b5be569587d65653655471a582f6d70
kubaparol <kubciop33@gmail.com>
2024-10-23 13:06:19 +0200

    ft: add construction status card to StatusCard component

---
+++ components/base/UnderConstructionPlaceholder.tsx
@@ -0,0 +1,19 @@
+import { ProjectUrls } from '@/constants';
+
+import StatusCard from '../shared/StatusCard';
+
+export default function UnderConstructionPlaceholder() {
+  return (
+    <div className="flex h-full flex-1 items-center justify-center">
+      <StatusCard
+        type="construction"
+        title="Coming Soon!"
+        message="We’re building something great. Stay tuned for updates!"
+        primaryAction={{
+          label: 'Go to Home',
+          href: ProjectUrls.home,
+        }}
+      />
+    </div>
+  );
+}
