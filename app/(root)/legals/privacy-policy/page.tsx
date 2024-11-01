8aa561863f9d6a3ace761d4625ec47fe3234d3f2
kubaparol <kubciop33@gmail.com>
2024-11-01 18:49:39 +0100

    ft: enhance Privacy Policy page layout and content; add smooth scrolling to global styles

--- app/(root)/legals/privacy-policy/page.tsx
+++ app/(root)/legals/privacy-policy/page.tsx
@@ -1,55 +1,136 @@
+import dayjs from 'dayjs';
+import { ArrowUp } from 'lucide-react';
 import { Metadata } from 'next';
+import Link from 'next/link';
+
+import { Separator } from '@/components/ui/separator';
 
 export const metadata: Metadata = {
   title: 'Privacy Policy',
 };
 
-const terms = [
-  {
-    title: 'Information We Collect',
-    description:
-      'We collect information that you provide to us directly, such as when you create an account, update your profile, or use interactive features of our services.',
-  },
-  {
-    title: 'How We Use Information',
-    description:
-      'We use the information we collect to provide, maintain, and improve our services, to develop new services, and to protect our company and our users.',
-  },
-  {
-    title: 'Sharing Information',
-    description:
-      'We do not share your personal information with third parties except as described in this policy. We may share information with vendors and service providers who perform services on our behalf.',
-  },
-  {
-    title: 'Your Rights',
-    description:
-      'You have the right to access, update, or delete your personal information. You can do this by contacting us directly or through your account settings.',
-  },
-  {
-    title: 'Contact Us',
-    description:
-      'If you have any questions about this privacy policy, please contact us at support@cookly-app.com.',
-  },
-];
-
 export default function PrivacyPolicyPage() {
   return (
-    <section className="wrapper grid gap-12">
-      <header>
-        <h1 className="mb-4 text-3xl font-bold">Privacy Policy</h1>
-        <p className="mt-1 max-w-2xl text-sm text-gray-500">
-          Last update: {new Date().toLocaleDateString()}
+    <section className="wrapper !mx-auto grid !max-w-6xl gap-12 !py-12">
+      <header className="grid place-items-center gap-4" id="top">
+        <h1 className="text-3xl font-semibold md:text-6xl">Privacy Policy</h1>
+        <p className="text-gray-500 md:text-lg">
+          Last updated {dayjs().subtract(2, 'week').format('MMMM D, YYYY')}
         </p>
       </header>
 
-      <ul className="gris gap-2">
-        {terms.map((term, index) => (
-          <li key={index}>
-            <h2 className="mb-2 text-2xl font-semibold">{term.title}</h2>
-            <p className="mb-4">{term.description}</p>
-          </li>
-        ))}
-      </ul>
+      <div className="grid gap-10 md:grid-cols-6">
+        <div className="grid gap-12 md:col-span-4">
+          <h2 className="text-xl font-semibold md:text-3xl">
+            This Privacy Policy will help you better understand how we collect, use and share your
+            personal information.
+          </h2>
+
+          <div className="grid gap-4">
+            <h3 className="text-xl font-semibold">Privacy Policy</h3>
+
+            {policy.map((item, index) => (
+              <p key={index} className="text-gray-500">
+                {item}
+              </p>
+            ))}
+          </div>
+
+          <div className="grid gap-10">
+            <h3 className="text-xl font-semibold">Privacy Summary</h3>
+
+            <ol className="grid gap-8">
+              {summary.map((item, index) => (
+                <li key={index} id={item.id} className="grid gap-3">
+                  <p className="font-semibold">
+                    {index + 1}. {item.title}
+                  </p>
+
+                  <p className="text-gray-500">{item.content}</p>
+                </li>
+              ))}
+            </ol>
+          </div>
+        </div>
+
+        <div className="flex h-fit flex-col gap-4 max-md:row-start-1 max-md:row-end-2 md:sticky md:top-32 md:col-span-2">
+          <h3 className="text-xl font-semibold">Table of Contents</h3>
+
+          <ul className="grid gap-2 px-2">
+            {summary.map((item, index) => (
+              <li key={index}>
+                {index + 1}.{' '}
+                <Link href={`#${item.id}`} className="text-sm hover:underline">
+                  {item.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+
+          <Separator />
+
+          <Link href="#top" className="flex items-center px-2 text-sm hover:underline">
+            Back to top <ArrowUp className="ml-2 size-4" />
+          </Link>
+        </div>
+      </div>
     </section>
   );
 }
+
+const policy = [
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur totam laboriosam dolorem, optio mollitia veniam praesentium eius, omnis perferendis est dicta quisquam nam provident quas cupiditate consequuntur! In, inventore blanditiis! Architecto deserunt omnis aliquid magni in. Perferendis perspiciatis incidunt ipsum optio doloremque pariatur? Natus non ipsum voluptatum ipsam autem officiis nostrum, qui iure quaerat iste corporis labore ex quam voluptatem!',
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur totam laboriosam dolorem, optio mollitia veniam praesentium eius, omnis perferendis est dicta quisquam nam provident quas cupiditate consequuntur! In, inventore blanditiis! Architecto deserunt omnis aliquid magni in. Perferendis perspiciatis incidunt ipsum optio doloremque pariatur? Natus non ipsum voluptatum ipsam autem officiis nostrum, qui iure quaerat iste corporis labore ex quam voluptatem!',
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur totam laboriosam dolorem, optio mollitia veniam praesentium eius, omnis perferendis est dicta quisquam nam provident quas cupiditate consequuntur! In, inventore blanditiis! Architecto deserunt omnis aliquid magni in. Perferendis perspiciatis incidunt ipsum optio doloremque pariatur? Natus non ipsum voluptatum ipsam autem officiis nostrum, qui iure quaerat iste corporis labore ex quam voluptatem!',
+];
+
+const summary = [
+  {
+    id: 'what-personal-information-we-collect',
+    title: 'What Personal Information we collect',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur totam laboriosam dolorem, optio mollitia veniam praesentium eius, omnis perferendis est dicta quisquam nam provident quas cupiditate consequuntur! In, inventore blanditiis! Architecto deserunt omnis aliquid magni in. Perferendis perspiciatis incidunt ipsum optio doloremque pariatur? Natus non ipsum voluptatum ipsam autem officiis nostrum, qui iure quaerat iste corporis labore ex quam voluptatem!',
+  },
+  {
+    id: 'how-we-use-your-personal-information',
+    title: 'How we use your Personal Information',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur totam laboriosam dolorem, optio mollitia veniam praesentium eius, omnis perferendis est dicta quisquam nam provident quas cupiditate consequuntur! In, inventore blanditiis! Architecto deserunt omnis aliquid magni in. Perferendis perspiciatis incidunt ipsum optio doloremque pariatur? Natus non ipsum voluptatum ipsam autem officiis nostrum, qui iure quaerat iste corporis labore ex quam voluptatem!',
+  },
+  {
+    id: 'how-we-share-your-personal-information',
+    title: 'How we share your Personal Information',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur totam laboriosam dolorem, optio mollitia veniam praesentium eius, omnis perferendis est dicta quisquam nam provident quas cupiditate consequuntur! In, inventore blanditiis! Architecto deserunt omnis aliquid magni in. Perferendis perspiciatis incidunt ipsum optio doloremque pariatur? Natus non ipsum voluptatum ipsam autem officiis nostrum, qui iure quaerat iste corporis labore ex quam voluptatem!',
+  },
+  {
+    id: 'how-we-protect-your-personal-information',
+    title: 'How we protect your Personal Information',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur totam laboriosam dolorem, optio mollitia veniam praesentium eius, omnis perferendis est dicta quisquam nam provident quas cupiditate consequuntur! In, inventore blanditiis! Architecto deserunt omnis aliquid magni in. Perferendis perspiciatis incidunt ipsum optio doloremque pariatur? Natus non ipsum voluptatum ipsam autem officiis nostrum, qui iure quaerat iste corporis labore ex quam voluptatem!',
+  },
+  {
+    id: 'how-long-we-keep-your-personal-information',
+    title: 'How long we keep your Personal Information',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur totam laboriosam dolorem, optio mollitia veniam praesentium eius, omnis perferendis est dicta quisquam nam provident quas cupiditate consequuntur! In, inventore blanditiis! Architecto deserunt omnis aliquid magni in. Perferendis perspiciatis incidunt ipsum optio doloremque pariatur? Natus non ipsum voluptatum ipsam autem officiis nostrum, qui iure quaerat iste corporis labore ex quam voluptatem!',
+  },
+  {
+    id: 'how-to-contact-us',
+    title: 'How to contact us',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur totam laboriosam dolorem, optio mollitia veniam praesentium eius, omnis perferendis est dicta quisquam nam provident quas cupiditate consequuntur! In, inventore blanditiis! Architecto deserunt omnis aliquid magni in. Perferendis perspiciatis incidunt ipsum optio doloremque pariatur? Natus non ipsum voluptatum ipsam autem officiis nostrum, qui iure quaerat iste corporis labore ex quam voluptatem!',
+  },
+  {
+    id: 'how-we-update-our-privacy-policy',
+    title: 'How we update our Privacy Policy',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur totam laboriosam dolorem, optio mollitia veniam praesentium eius, omnis perferendis est dicta quisquam nam provident quas cupiditate consequuntur! In, inventore blanditiis! Architecto deserunt omnis aliquid magni in. Perferendis perspiciatis incidunt ipsum optio doloremque pariatur? Natus non ipsum voluptatum ipsam autem officiis nostrum, qui iure quaerat iste corporis labore ex quam voluptatem!',
+  },
+  {
+    id: 'how-to-opt-out',
+    title: 'How to opt out',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur totam laboriosam dolorem, optio mollitia veniam praesentium eius, omnis perferendis est dicta quisquam nam provident quas cupiditate consequuntur! In, inventore blanditiis! Architecto deserunt omnis aliquid magni in. Perferendis perspiciatis incidunt ipsum optio doloremque pariatur? Natus non ipsum voluptatum ipsam autem officiis nostrum, qui iure quaerat iste corporis labore ex quam voluptatem!',
+  },
+];
