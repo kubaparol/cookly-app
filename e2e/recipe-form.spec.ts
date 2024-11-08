55da04ef72c37f06cc116f6bef3a282681c360bf
kubaparol <kubciop33@gmail.com>
2024-11-08 19:32:51 +0100

    ft: enhance recipe form e2e tests with improved login flow and file upload handling

--- e2e/recipe-form.spec.ts
+++ e2e/recipe-form.spec.ts
@@ -1,4 +1,5 @@
 import test, { expect } from '@playwright/test';
+import path from 'path';
 
 import { ProjectUrls } from '@/constants';
 
@@ -15,43 +16,44 @@ const recipe = {
 };
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('http://localhost:3000');
-
-  const dashboardButton = page.getByRole('link', { name: 'Dashboard' });
-
-  if (dashboardButton) {
-    await page.goto(`http://localhost:3000${ProjectUrls.createRecipe}`);
-
-    return;
-  }
-
-  await page.getByRole('link', { name: 'Login' }).click();
+  await page.goto(`http://localhost:3000${ProjectUrls.createRecipe}`);
 
   const email = page.getByLabel('Email address');
+  await email.fill(process.env.E2E_CLERK_USER_USERNAME!);
 
   await page.getByRole('button', { name: 'Continue' }).click();
 
   const password = page.getByLabel('Password', { exact: true });
+  await password.fill(process.env.E2E_CLERK_USER_PASSWORD!);
 
   await page.getByRole('button', { name: 'Continue' }).click();
-
   await page.goto(`http://localhost:3000${ProjectUrls.createRecipe}`);
+
+  await page.reload();
 });
 
 test.describe('Recipe Form', () => {
   test('should allow the user to create a new recipe', async ({ page }) => {
+    // title
     const title = page.getByPlaceholder('e.g., Classic Spaghetti');
     await title.fill(recipe.title);
+    //
 
+    // description
     const recipeDescription = page.getByPlaceholder('e.g., A rich and hearty');
     if (recipe.description) {
       await recipeDescription.fill(recipe.description);
     }
+    //
 
-    const image = page.getByRole('button', { name: 'Select from computer' });
-    await image.setInputFiles('https://utfs.io/f/tn9qWFoW4N1fTrsshufj02R3gc8GJtPrsQjC9qLAuXFnziYo');
+    // image
+    const fileChooserPromise = page.waitForEvent('filechooser');
+    await page.getByText('Select from computer').click();
+    const fileChooser = await fileChooserPromise;
+    await fileChooser.setFiles(path.join(__dirname, '../public/beef.jpg'));
+    //
 
-    // ingredient form
+    // ingredient
     await page.getByRole('button', { name: 'Add Ingredient' }).click();
 
     const quantity = page.getByPlaceholder('e.g., 100');
@@ -72,7 +74,7 @@ test.describe('Recipe Form', () => {
     );
     //
 
-    // step form
+    // step
     await page.getByRole('button', { name: 'Add Step' }).click();
 
     const stepDescription = page.getByPlaceholder('e.g., Preheat the oven to 180');
@@ -88,7 +90,7 @@ test.describe('Recipe Form', () => {
     const submitButton = page.getByRole('button', { name: 'Create Recipe' });
     await submitButton.click();
 
-    await expect(submitButton).toBeDisabled();
+    await expect(page.locator('h1')).toHaveText('Success!');
   });
 
   test('should display an error message if the form is submitted with missing fields', async ({
@@ -97,7 +99,7 @@ test.describe('Recipe Form', () => {
     const submitButton = page.getByRole('button', { name: 'Create Recipe' });
     await submitButton.click();
 
-    const errorMessage = page.getByTestId('error-message');
+    const errorMessage = page.getByTestId('error-message').first();
 
     await expect(errorMessage).toBeVisible();
   });
